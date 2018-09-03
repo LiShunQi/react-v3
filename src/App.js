28d584ef6bfe29d945e4d39a1b9dac7d3f6e42bb
LiShunQi <ggshun7@126.com>
2018-09-03 16:30:26 +0800

    chore: ok

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <Provider store={store}>
           <div className="App">
               <nav>
-                  <Link to="/">Home</Link> |{" "}
+                  <Link to="/">Home 6666</Link> |{" "}
                   <Link to="/about/123">about</Link>
               </nav>
               <Router>
